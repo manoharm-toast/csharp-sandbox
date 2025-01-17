cd6b83110fe97348286ece222a92c98facd3645b
Mano <89192640+manoharm-toast@users.noreply.github.com>
2025-01-17 19:26:25 +0530

    Middleware done by using the param

--- middle-ware-xyz/middle-ware-xyz/Middleware.cs
+++ middle-ware-xyz/middle-ware-xyz/Middleware.cs
@@ -5,31 +5,38 @@ namespace xyz.middleware
         public void consoleWrite(dynamic value){
             Console.WriteLine(value);
         }
-        public void FirstMethod()
+        public void FirstMethod(string name)
         {
-            consoleWrite("this my first method");
+            consoleWrite($"this my first method : {name}");
         }
 
-        public void SecondMethod()
+        public void SecondMethod(string name)
         {
-            consoleWrite("this my second method");
+            consoleWrite($"this my second method : {name}");
         }
 
-       
+        
         public void Wrapper(Action function){
             consoleWrite("starts here");
             function();
             consoleWrite("ends here");
         }
 
-        public void Try(Action function){
-            function();
+        public void Try(string param1 ,Action<string> function){
+            function(param1);
         }
 
 
          public void main(){
-            Try(() => Wrapper(FirstMethod));
-            Try(() => Wrapper(SecondMethod));        
+            Wrapper(() => {
+                FirstMethod("Smith");
+                SecondMethod("Steve");
+            });
+
+            Try("mano", (name) => {
+                FirstMethod(name);
+                SecondMethod(name);
+            });
         }
     }
 }
